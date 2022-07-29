e018fe3d9c8890e12fa74ea202f03273f005231e
AntonKopylov89 <ttxb@yandex.ru>
2022-07-29 17:29:36 +0400

    Add tests

--- src/test/java/CardFormTest.java
+++ src/test/java/CardFormTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardFormTest {
@@ -19,13 +20,23 @@ public class CardFormTest {
     }
 
     @Test
+    void shouldSubmitRequestWithDoubleSurname() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Римский-Корсаков Евгений");
+        $("[data-test-id=phone] input").setValue("+79578664311");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
     void shouldNameError() {
         open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Bill Snow");
         $("[data-test-id=phone] input").setValue("+79578664311");
         $("[data-test-id=agreement]").click();
         $(".button").click();
-        $(".input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
@@ -35,7 +46,7 @@ public class CardFormTest {
         $("[data-test-id=phone] input").setValue("79578664311");
         $("[data-test-id=agreement]").click();
         $(".button").click();
-        $(".input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
@@ -44,7 +55,7 @@ public class CardFormTest {
         $("[data-test-id=phone] input").setValue("+79578664311");
         $("[data-test-id=agreement]").click();
         $(".button").click();
-        $(".input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -53,7 +64,16 @@ public class CardFormTest {
         $("[data-test-id=name] input").setValue("Иванов Евгений");
         $("[data-test-id=agreement]").click();
         $(".button").click();
-        $(".input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldErrorWithoutCheckbox() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Иванов Евгений");
+        $("[data-test-id=phone] input").setValue("+79578664311");
+        $(".button").click();
+        $("[data-test-id='agreement'].input_invalid").should(exist);
     }
 
     @AfterEach
